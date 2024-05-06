2dbc165588a28eb096e286af086413235912f592
Stéphane Bard <stephane.bard@gmail.com>
2024-05-06 22:37:02 +0200

    add ws+wss new endpoint to avoid polling, upgrade requirements to python 3.11 and Dockerfile as well

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,33 @@
-FROM python:3.9
+FROM python:3.11.9-bookworm
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+    patch
 
 WORKDIR /code
 
-COPY ./requirements.txt /code/requirements.txt
+COPY ./requirements_311.txt /code/requirements.txt
 
+# Get Ubuntu packages
+RUN apt-get install -y \
+    build-essential \
+    curl
+
+# Update new packages
+RUN apt-get update
+
+# Get Rust
+RUN curl https://sh.rustup.rs -sSf | bash -s -- -y
+
+RUN echo 'source $HOME/.cargo/env' >> $HOME/.bashrc
+ENV PATH="/root/.cargo/bin:${PATH}"
+
+RUN pip install --upgrade pip
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+RUN rustup self uninstall -y
+
+COPY patches/aredis_python_3_10.patch aredis.patch
+RUN patch -p1 /usr/local/lib/python3.11/site-packages/aredis/connection.py  < aredis.patch
 
 COPY ./app /code/app
 
--- app/main.py
+++ app/main.py
@@ -1,35 +1,87 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Copyright (C) 2021  Stéphane Bard <stephane.bard@gmail.com>
-#
-# This file is part of deaddrop
-#
-# deaddrop is free software; you can redistribute it and/or modify it under
-# the terms of the M.I.T License.
-#
-from typing import Optional, List
-from datetime import datetime
-from fastapi import FastAPI, Request, Response, Header, HTTPException
+"""
+ Copyright (C) 2024  Stéphane Bard <stephane.bard@gmail.com>
+
+ This file is part of deaddrop
+
+ deaddrop is free software; you can redistribute it and/or modify it under
+ the terms of the M.I.T License.
+"""
+
+from typing import List, Annotated
+import asyncio
 import uuid
 import os
-import pickle
+import json
+from datetime import datetime
+
+from fastapi import (
+    FastAPI,
+    Request,
+    Response,
+    Header,
+    HTTPException,
+    WebSocket,
+    WebSocketException,
+    status,
+)
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.params import Header as HeaderType
 from aredis import StrictRedis
 
 # set redis for scalability
-redis_host   = os.getenv("REDIS_HOST_NAME", "127.0.0.1")
-redis_port   = int(os.getenv("REDIS_TCP_PORT", 6379))
-redis_db     = int(os.getenv("REDIS_DEFAULT_DEB", 0))
+redis_host = os.getenv("REDIS_HOST_NAME", "127.0.0.1")
+redis_port = int(os.getenv("REDIS_TCP_PORT", "6379"))
+redis_db = int(os.getenv("REDIS_DEFAULT_DEB", "0"))
 
 app = FastAPI(redoc_url=None, docs_url=None)
 client: StrictRedis = StrictRedis(host=redis_host, port=redis_port, db=redis_db)
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+
 class UnknownToken(HTTPException):
-    ...
+    """
+    exception raised if token is unknown
+    """
+
+
+@app.websocket("/tokens/{token_id}/ws")
+async def websocket_endpoint(websocket: WebSocket, token_id: str):
+    """
+        websocket endpoint if token is known
+    """
+    data = await client.get(token_id)
+    if data is None:
+        raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
+
+    await websocket.accept()
+    data = json.loads(data)
+    while not data:
+        serialized_data = await client.get(token_id)
+        data = json.loads(serialized_data)
+        if data:
+            await websocket.send_text(f"{data}")
+            break
+        await asyncio.sleep(0.01)
+    else:
+        await websocket.send_text(f"{data}")
+    await websocket.close()
 
 
 async def default_route(scope, receive, send):
+    """
+    a default route for K8s.
+    simple heart beat
+    """
     response = Response("Ok")
     await response(scope, receive, send)
 
@@ -38,25 +90,32 @@ app.router.default = default_route
 
 
 async def add_token(token: str) -> None:
-    """ """
-    await client.set(token, pickle.dumps([]), 3600)
+    """
+    create token on REDIS
+    """
+    await client.set(token, json.dumps([]), 3600)
 
 
-async def save_data(token: str, method: str, data: str, headers: List):
-    """ """
+async def save_data(
+    token: str, method: str, data: str, headers: List, user_agent: str | None = None
+):
+    """
+    Save data on the corresponding token
+    """
     now = datetime.now()
     payload = {
         "created_at": now.strftime("%Y-%m-%d %H:%M:%S"),
         "method": method,
-        "content": data.decode("utf-8"),
+        "content": data,
         "headers": headers,
+        "user_agent": user_agent,
     }
 
     data = await client.get(token)
     if data:
-        data = pickle.loads(data)
+        data = json.loads(data)
         data.append(payload)
-        await client.set(token, pickle.dumps(data), 3600)
+        await client.set(token, json.dumps(data), 3600)
     else:
         raise UnknownToken(status_code=404, detail="unknown")
 
@@ -68,11 +127,19 @@ async def get_content(token):
     """
     data = await client.get(token)
     if data:
-        data = pickle.loads(data)
+        data = json.loads(data)
         result = {"data": data}
     else:
         raise UnknownToken(status_code=404, detail="unknown")
     return result
+
+
+@app.get("/health", status_code=218)
+async def healthcheck():
+    """
+    validate that the app is up and running
+    """
+    return Response("Ok")
 
 
 @app.post("/token")
@@ -90,13 +157,22 @@ async def create_token():
 @app.put("/{token:str}")
 @app.get("/{token:str}")
 @app.post("/{token:str}")
-async def read_item(
-    token: str, request: Request, user_agent: Optional[str] = Header(None)
+async def dead_drop_on_token(
+    token: str, request: Request, user_agent: Annotated[str | None, Header()] = None
 ):
+    """
+    receives a request (whatever verb) and will extract data from this request
+    and attach these data on related token (the one extract from URL path)
+    """
     content = await request.body()
-    headers = request.headers.raw
+    content = content.decode("utf-8")
+    headers = [(h.decode("utf-8"), v.decode("utf-8")) for (h, v) in request.headers.raw]
     method = request.method
     method = method.upper()
 
-    await save_data(token, method, content, headers)
+    if isinstance(user_agent, HeaderType):
+        # ensure data will be a string
+        user_agent = repr(user_agent)
+
+    await save_data(token, method, content, headers, user_agent=user_agent)
     return "Ok"
